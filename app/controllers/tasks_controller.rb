26750fc2fb816f28971ff1347853d4faa232342e
taro.program <thisisforstudyingprogram@gmail.com>
2019-11-02 07:56:35 +0000

    loggin function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+    @task = current_user.tasks.build
+    @tasks = current_user.tasks.order(id: :desc)
+    
+    end
   end
     
   def show
@@ -13,12 +18,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に登録されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Taskが登録されませんでした'
       render :new
     end
